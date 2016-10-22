bf73ffb21e3d490954b7d7f3b4ab1ed93c107be0
Jordi Fernandez <jordi@ywarp.com>
2016-10-22 13:04:10 +0100

    not sure about changes

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,3 +1,9 @@
+//= require jednotka/jquery/jquery.min
+//= require jednotka/jquery/jquery.mobile.custom.min
+//= require jednotka/bootstrap/bootstrap.min
+//= require jednotka/plugins/_plugins
+//= require jednotka/jednotka
+
 //= require jquery
 //= require jquery_ujs
 //= require bootstrap-sprockets
