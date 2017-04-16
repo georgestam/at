50d0ea31fb568a14ef10855dcdd636cef7157822
Jordi Fernandez <jordi@ywarp.com>
2017-04-16 20:23:08 +0200

    fix meta tags to display info of the articles

--- app/helpers/meta_tags_helper.rb
+++ app/helpers/meta_tags_helper.rb
@@ -10,4 +10,16 @@ module MetaTagsHelper
   def meta_image
     content_for?(:meta_image) ? content_for(:meta_image) : DEFAULT_META['meta_image']
   end
+  
+  def meta_blog_title
+    @article.title
+  end
+
+  def meta_blog_description
+    @article.text.html_safe
+  end
+
+  def meta_blog_image
+    @article.photo
+  end
 end
